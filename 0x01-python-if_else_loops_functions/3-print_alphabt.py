eac1aa69de50fa8378d17d4a8e5e6469f1e44e83
Kgothatso H. Khoza <kgothikhoz@gmail.com>
2023-11-29 05:13:24 -0800

    3rd push

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for character in range(ord('a'), ord('z') + 1):
     if character not in [ord('e'), ord('q')]:
-        print(chr(character), end='')
+        print(f"{chr(character)}", end='')
