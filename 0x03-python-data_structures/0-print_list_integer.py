81c6b551bb7b94f558b833efcbe3b2d3b561a22b
Kgothatso H. Khoza <kgothikhoz@gmail.com>
2023-12-04 12:08:23 -0800

    Data Structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+from typing import List, Optional
 
-from typing import List
-
-def print_list_integer(_list: List[int] = None) -> None:
+def print_list_integer(_list: Optional[List[int]] = None) -> None:
     if _list is None:
         _list = []
-    
+
     for i in _list:
         print(f'{i:d}')
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
