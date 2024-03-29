885006bfe3ff955f12c555baf7c51bf10f572a18
KgothatsoHUB <kgothikhoza@gmail.com>
2024-03-28 17:07:43 -0700

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a request, and displays the size of the response body in bytes
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 khoza.tech"
-    exit 1
-fi
-
-# Send a request to the URL and count the size of the response body in bytes
-curl -s "$1" | wc -c
-
+# Sends a request to that URL, and displays the size of the body of the response
+curl -s "${1}" | wc -c
